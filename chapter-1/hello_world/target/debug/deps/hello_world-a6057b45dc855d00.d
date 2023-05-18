b0c1d96438ac59329fc37dbc230fa5ee61ab3800
V4yukon <yxyvan@gmail.com>
2023-05-18 15:46:41 +0800

    chapter1

---
+++ chapter-1/hello_world/target/debug/deps/hello_world-a6057b45dc855d00.d
@@ -0,0 +1,5 @@
+/rust-project/chapter-1/hello_world/target/debug/deps/hello_world-a6057b45dc855d00: src/main.rs
+
+/rust-project/chapter-1/hello_world/target/debug/deps/hello_world-a6057b45dc855d00.d: src/main.rs
+
+src/main.rs:
---
+++ chapter-1/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/rust-project/chapter-1/hello_world/target/debug/hello_world: /rust-project/chapter-1/hello_world/src/main.rs
