9f30b728f90302472682f7b041768391940ee47b
V4yukon <yxyvan@gmail.com>
2023-05-27 11:25:14 +0800

    finished learning of chapter10

---
+++ chapter-10/vali-ref/target/debug/deps/vali_ref-a80144ded35d4bf7.d
@@ -0,0 +1,5 @@
+/rust-project/chapter-10/vali-ref/target/debug/deps/vali_ref-a80144ded35d4bf7: src/main.rs
+
+/rust-project/chapter-10/vali-ref/target/debug/deps/vali_ref-a80144ded35d4bf7.d: src/main.rs
+
+src/main.rs:
---
+++ chapter-10/vali-ref/target/debug/vali-ref.d
@@ -0,0 +1,1 @@
+/rust-project/chapter-10/vali-ref/target/debug/vali-ref: /rust-project/chapter-10/vali-ref/src/main.rs
