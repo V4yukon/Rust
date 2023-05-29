9c1327b1d7e832b19f8495ea489083d68a4bc8f5
V4yukon <yxyvan@gmail.com>
2023-05-29 10:25:22 +0800

    use extern crate diesel to connect psql or mysql

---
+++ sql-practice/target/debug/deps/sql_practice-c0a0d447b48a5e97.d
@@ -0,0 +1,5 @@
+/rust-dev/Rust/sql-practice/target/debug/deps/sql_practice-c0a0d447b48a5e97.rmeta: src/main.rs
+
+/rust-dev/Rust/sql-practice/target/debug/deps/sql_practice-c0a0d447b48a5e97.d: src/main.rs
+
+src/main.rs:
