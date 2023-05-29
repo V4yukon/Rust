80a0d4840fbcb9048af7adb255a2b6306b331812
V4yukon <yxyvan@gmail.com>
2023-05-29 22:36:33 +0800

    Several ways to write tests

--- chapter-11/adder/src/lib.rs
+++ chapter-11/adder/src/lib.rs
@@ -45,6 +45,28 @@ mod tests {
 
         assert!(!smaller.can_hold(&lager));
     }
+    #[test]
+   // #[should_panic]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+
+
+
+
+    //
+    //using Result<T, E> in test
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
 
 
     
@@ -61,6 +83,36 @@ struct Rectangle {
 impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+}
+
+
+////
+////check panic with should_panic
+////
+////
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        //if value < 1 || value > 100 {
+          //  panic!("Guess value must be between 1 and 100 , got {}.", value);
+        //}
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+                );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+                );
+        }
+
+        Guess {value}
     }
 }
 
