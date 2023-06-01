127a1a9eafde41e55810599753c4c7bc6d5d1b67
V4yukon <yxyvan@gmail.com>
2023-06-01 14:17:00 +0800

    delete makefile

--- sql-practice/target/debug/deps/sql_practice-c0a0d447b48a5e97.d
+++
@@ -1,5 +0,0 @@
-/rust-dev/Rust/sql-practice/target/debug/deps/sql_practice-c0a0d447b48a5e97.rmeta: src/main.rs
-
-/rust-dev/Rust/sql-practice/target/debug/deps/sql_practice-c0a0d447b48a5e97.d: src/main.rs
-
-src/main.rs:
