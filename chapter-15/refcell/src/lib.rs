f7f49f1dbfce4fd38cfdb8b5820031aaf8936a09
V4yukon <yxyvan@gmail.com>
2023-06-09 15:59:47 +0800

    Interior Mutability

--- chapter-15/refcell/src/lib.rs
+++ chapter-15/refcell/src/lib.rs
@@ -39,22 +39,31 @@ where
 
 mod tests {
     use super::*;
+    use std::cell::RefCell;
+
 
     struct MockMessenger {
-        sent_messages: Vec<String>,
+        sent_messages: RefCell<Vec<String>>,
     }
 
     impl MockMessenger {
         fn new() -> MockMessenger {
             MockMessenger {
-                sent_messages: vec![],
+                sent_messages: RefCell::new(vec![]),
             }
         }
     }
 
     impl Messenger for MockMessenger {
         fn send(&self, message: &str) {
-            self.sent_messages.push(String::from(message));
+            let mut one_borrow = self.sent_messages.borrow_mut();
+            let mut two_borrow = self.sent_messages.borrow_mut();
+            
+            one_borrow.push(String::from(message));
+            two_borrow.push(String::from(message));
+
+
+            //self.sent_messages.borrow_mut().push(String::from(message));
         }
     }
 
@@ -66,6 +75,6 @@ mod tests {
 
         limit_tracker.set_value(80);
 
-        assert_eq!(mock_messager.sent_messages.len(), 1);
+        assert_eq!(mock_messager.sent_messages.borrow().len(), 1);
     }
 }
