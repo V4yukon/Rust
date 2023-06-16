84110d2fc62b2e31b44e59068fd7cbc60c2c63e8
V4yukon <yxyvan@gmail.com>
2023-06-16 23:00:51 +0800

    algorithms

---
+++ practice/algorithms/src/backtracking/all_combination_of_size_k.rs
@@ -0,0 +1,11 @@
+/*
+ *
+ * In this problem, we want to determine all possible combinations of k numbers out of 1 ... n . we
+ * use backtracking to solve this problem.
+ * Time complexity: O(C(n, k)) which is 0(n choose k) = O((n!/(k! * (n-k)!)))
+ *
+ *
+ * generate_all_combinations(n=4, k=2) => [[1,2], [1,3], [1,4], [2,3], [2,4], [3,4]]
+ */
+
+
