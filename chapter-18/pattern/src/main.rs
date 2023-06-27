3bdfc1188debe7f1c6585a951d304677c084c442
V4yukon <yxyvan@gmail.com>
2023-06-27 21:34:38 +0800

    hello

---
+++ chapter-18/pattern/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x = 15;
+
+    match x {
+        None => None,
+        Some(x) => Some(x + 1),
+    };
+
+}
---
+++ chapter-18/pattern/target/debug/deps/pattern-f094075c8a353479.d
@@ -0,0 +1,5 @@
+/rust-dev/Rust/chapter-18/pattern/target/debug/deps/pattern-f094075c8a353479.rmeta: src/main.rs
+
+/rust-dev/Rust/chapter-18/pattern/target/debug/deps/pattern-f094075c8a353479.d: src/main.rs
+
+src/main.rs:
