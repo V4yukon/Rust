95f9c862254be8825c11d5069b66d75efb546a55
V4yukon <yxyvan@gmail.com>
2023-06-28 23:01:13 +0800

    go home

--- chapter-18/pattern/target/debug/deps/pattern-f094075c8a353479.d
+++
@@ -1,5 +0,0 @@
-/rust-dev/Rust/chapter-18/pattern/target/debug/deps/pattern-f094075c8a353479.rmeta: src/main.rs
-
-/rust-dev/Rust/chapter-18/pattern/target/debug/deps/pattern-f094075c8a353479.d: src/main.rs
-
-src/main.rs:
