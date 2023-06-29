2afeeca71e0741cbc67b7152ca2f13455de2c464
V4yukon <yxyvan@gmail.com>
2023-06-29 17:22:06 +0800

    last day in 25.11

--- chapter-18/pattern/src/main.rs
+++ chapter-18/pattern/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
     let favorite_color: Option<&str> = None;
     let is_tuesday = false;
-    let age: Result<u8, _> = "34".parse();
+    let age: Result<u8, _> = "30".parse();
+
 
     if let Some(color) = favorite_color {
         println!("Using your favorite color, {color}, as the background");
@@ -16,4 +17,23 @@ fn main() {
     } else {
         println!("Using blue as the background color");
     }
+
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    let (x, y, z) = (1, 4, 7);
+    println!("{}", y);
 }
