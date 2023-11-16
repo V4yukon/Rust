ba670505ddf559b80169f319fffaff4a9dc284e6
V4yukon <yxyvan@gmail.com>
2023-11-16 12:19:03 +0800

    box

--- review_code/smart-point/src/main.rs
+++ review_code/smart-point/src/main.rs
@@ -7,10 +7,36 @@ fn fpp() -> Box<i32> {
     Box::new(s)
 }
 
+#[derive(Debug)]
+struct Point {
+    x: u32, 
+    y: u32,
+}
+
+impl Point {
+    fn play(&self) {
+        println!("I'm a method of Point");
+    }
+}
+
+
+fn fqq() -> Box<Point> {
+    let p = Point {x: 10, y: 20};
+    Box::new(p)
+}
+
 fn main() {
     let _i = foo();
     let _s = fpp();
 
     println!("{:?}", _i);
     println!("{:?}", _s);
+    
+
+    let boxed: Box<Point> = Box::new(Point{x: 10, y: 20});
+    boxed.play();
+    println!("{:?}", boxed);
+
+
+    
 }
