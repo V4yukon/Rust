5756348a1ba0f82cbe21dc000187326fc03e241f
V4yukon <yxyvan@gmail.com>
2023-11-17 15:54:10 +0800

    update some task and spawn

--- review_code/async-code/src/main.rs
+++ review_code/async-code/src/main.rs
@@ -1,4 +1,24 @@
+use tokio::task;
+
+async fn my_background_op(id: i32) -> String {
+    let s = format!("Starting background task {}.", id);
+    println!("{}", s);
+    s
+}
+
 #[tokio::main]
-async fn main() {
-    println!("Hello world!");
+
+async  main() {
+    let ops =vec![1, 2, 3];
+    let mut tasks = Vec::with_capacity(ops.len());
+    for op in ops {
+        tasks.push(tokio::spawn(my_background_op(op)));
+    }
+
+    let mut outputs = Vec::with_capacity(tasks.len());
+    for task in tasks {
+        outputs.push(task.await.unwrap());
+    }
+
+    println!("{:?}", outputs);
 }
