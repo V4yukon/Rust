7b8f71836bfacf9b615eaf155359995f4413a80e
V4yukon <yxyvan@gmail.com>
2023-11-21 10:48:08 +0800

    tcp server and client

---
+++ review_code/getinf/src/server.rs
@@ -0,0 +1,62 @@
+use std::env;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+use tokio::process::Command;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+    println!("Listening on: {}", addr);
+    let listener = TcpListener::bind(&addr).await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            let mut offset = 0;
+
+            loop {
+                let n = socket
+                    .read(&mut buf[offset..])
+                    .await
+                    .expect("failed to read data from socket");
+
+                if n == 0 {
+                    return;
+                }
+
+                println!("offset: {offset}, n: {n}");
+
+                let end = offset + n;
+
+                if let Ok(directive) = std::str::from_utf8(&buf[..end]) {
+                    println!("{directive}");
+
+                    let output = process(directive).await;
+
+                    println!("{output}");
+
+                    socket
+                        .write_all(&output.as_bytes())
+                        .await
+                        .expect("failed to write data to socket");
+                } else {
+                    offset = n;
+                }
+                }
+        });
+    }
+}
+
+
+async fn process(directive: &str) -> String {
+    if directive == "gettime" {
+        let output = Command::new("date").output().await.unwrap();
+        String::from_utf8(output.stdout).unwrap()
+    } else { 
+        "invalid command".to_owned()
+    }
+}
