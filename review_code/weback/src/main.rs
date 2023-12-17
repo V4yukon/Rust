faf17e8552a84b195859db99a3915ebc0101057e
V4yukon <yxyvan@gmail.com>
2023-12-17 11:14:56 +0800

    update static resources

--- review_code/weback/src/main.rs
+++ review_code/weback/src/main.rs
@@ -4,6 +4,14 @@ use axum::{response::Html, routing::get, Router};
 
 async fn main() {
     let app = Router::new().route("/", get(handler));
+    let serve_dir = ServeDir::new("assets2")
+    .not_found_service(ServeFile::new("assets2/index.html"));
+    
+    let app = Router::new()
+        .route("/foo", get(handler))
+        .nest_service("assets", ServeDir::new("assets"))
+        .nest_service("/assets2", serve_dir.clone())
+        .fallback_service(serve_dir);
 
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
